7751a22cf726973974b559aca5f11f34f6c07307
sneakwish <wishal_wishnu@hotmail.com>
2021-09-01 18:07:17 +0200

    yo2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,16 +11,35 @@ public class Main {
             BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
 
             String s;
-            while (!(s = in.readLine()).equals(""))
+            s = in.readLine();
                 System.out.println(s);
             OutputStream out = client.getOutputStream();
             String world = "<a href=\"https://www.google.nl\">World!</a>";
-            String about = "<a href=\"/about\">About!</a>";
-            if(s.equals("HTTP /about"))
-            out.write(("HTTP/1.0 200 OK\r\n" +
-                    "Content-Type: text/html\r\n" +
-                    "Content-Length: \r\n\r\n" +
-                    "Hello " + world + about).getBytes());
+            String about = "Lees de<a href=\"/about\">About</a> Pagina.";
+            String counter = "\n\rLees de<a href=\"/counter\"> Counter</a> Pagina.";
+            if(s.equals("GET / HTTP/1.1")){
+                out.write(("HTTP/1.0 200 OK\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: \r\n\r\n" +
+                        "Hello " + world + about + counter).getBytes());
+            }
+            else if (s.equals("GET /about HTTP/1.1")){
+                out.write(("HTTP/1.0 200 OK\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: \r\n\r\n" +
+                        "ABOUT PAGINA " + world).getBytes());
+            }
+
+            else if (s.equals("GET /counter HTTP/1.1")){
+                out.write(("HTTP/1.0 200 OK\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: \r\n\r\n" +
+                        "ABOUT PAGINA " + world).getBytes());
+            }
+
+            else{
+                out.write(("FAiled").getBytes());
+            }
 
             in.close();
             out.close();
